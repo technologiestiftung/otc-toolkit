9464258f65b1910f18a3039b53d98fa8cca2ac8d
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-06-29 16:15:50 +0200

    add some output to ffmpeg script

--- recording/ffmpeg_recording.sh
+++ recording/ffmpeg_recording.sh
@@ -7,8 +7,10 @@ function main() {
   #Start recording
   #echo "recording starting now"
   cd "$1"
+  echo "moved to $1"
 
   FILENAME="$(date '+%Y-%m-%d-%H-%M-%S-%6N')"
+  echo "creating file $FILENAME"
   ffmpeg -use_wallclock_as_timestamps 1 -f mjpeg -i "http://localhost:8080/webcam/stream" -t 00:00:10 -c:v copy -y "${FILENAME}.mp4"
 
 }
