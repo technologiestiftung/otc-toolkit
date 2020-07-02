676c1b3b94831a8e5c06ce120003dbba0963a904
tsboter <roboter@tsb.ara.uberspace.de>
2020-07-02 14:23:47 +0200

    add archive script to main script

--- recording/record.sh
+++ recording/record.sh
@@ -9,20 +9,23 @@ IFS=$'\n\t'
 # $2    second argument is the folder
 #       where the recordings get stored
 # $3    Is the path where the record.sh is stored
+# $4    the path to the folder (or drive) where the zip archives
+#       should be moved
 #
 function main() {
-
+  TIMESTAMP="$(date '+%Y-%m-%d-%H-%M-%S-%6N')"
+  echo "Timestamp: $TIMESTAMP"
   cd "$3"
 
   ./start_odc.sh "$1"
 
   sleep 60
 
-  python3.7 ./init_odc.py
+  /usr/bin/python3.7 ./init_odc.py
 
   sleep 10
 
-  python3.7 ./start_odc_recording.py
+  /usr/bin/python3.7 ./start_odc_recording.py
 
   sleep 10
 
@@ -30,11 +33,11 @@ function main() {
 
   sleep 30
 
-  python3.7 ./stop_odc_recording.py
+  /usr/bin/python3.7 ./stop_odc_recording.py
 
   sleep 5
 
-  python3.7 ./download_tracker_data.py "$2"
+  /usr/bin/python3.7 ./download_tracker_data.py "$2"
 
   sleep 5
 
@@ -44,5 +47,9 @@ function main() {
 
   ./stop_odc.sh "$1"
 
+  sleep 5
+
+  ./archive-recordings.sh "$2" "$4"
+
 }
-main "$1" "$2" "$3"
+main "$1" "$2" "$3" "$4"
