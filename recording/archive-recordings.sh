6a1868e90e8d476f58d370adced28be5ba60d793
tsboter <roboter@tsb.ara.uberspace.de>
2020-07-02 14:23:16 +0200

    add archive script

---
+++ recording/archive-recordings.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# this script expects as the first argument the path to the outputfolder
+# where the recordings are stored
+# as second argument the path where the archives should be moved
+#
+function main (){
+  for f in $(find "$1" -mindepth 1 -maxdepth 1 -type d ); do
+    echo "archiving folder: $f"
+    zip -r -q -T -j "$f" "$f"
+    echo "moving archive ''$f.zip' to '$2'"
+    mv "$f.zip" "$2"
+    echo "removing folder: $f"
+    rm -rf "$f"
+    echo "---"
+    echo ""
+  done
+}
+
+main "$1" "$2"
