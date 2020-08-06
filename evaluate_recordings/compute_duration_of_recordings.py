ace4a023f20fb4fdc24065dc6837096525e66152
Helena Mihaljevic <helena.m-b@t-online.de>
2020-08-06 11:24:31 +0200

    add some more evaluation of frameId in relation to timestamp to tracker json file

--- evaluate_recordings/compute_duration_of_recordings.py
+++ evaluate_recordings/compute_duration_of_recordings.py
@@ -2,6 +2,9 @@
 from glob import glob
 from os.path import join
 
+import pytz
+
+utc = pytz.utc
 import pandas as pd
 
 from config import PATH_TO_RECORDINGS
@@ -13,5 +16,12 @@ tracker_data_files = glob(join(PATH_TO_RECORDINGS, "**/*_tracker.json"))
 for f in tracker_data_files:
     print(f"path to JSON file: {f}")
     tracker_data = pd.read_json(f)
-    print(f"number of frames in tracker JSON: {len(tracker_data)}")
+    # print(tracker_data.shape)
+    print(f"number of rows tracker JSON: {len(tracker_data)}")
+    print(f"number of distinct frames in tracker JSON: {tracker_data.frameId.nunique()}")
+
+    frames = tracker_data.groupby("frameId").aggregate(list)
+    frames.timestamp = frames.timestamp.map(lambda x: set(x))
+    print(f"number of timestamps per frame: {frames.timestamp.map(lambda x: len(x)).value_counts()}")
+
     print(f"tracking duration: {tracker_data['timestamp'].max() - tracker_data['timestamp'].min()}")
