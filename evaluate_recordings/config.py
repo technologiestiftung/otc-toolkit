ed2bff15995849ce938975bc475c2939eb68c4f1
Helena Mihaljevic <helena.m-b@t-online.de>
2020-09-15 20:43:32 +0200

    Add variable for counter line coordinates; currently only citylab TX2

--- evaluate_recordings/config.py
+++ evaluate_recordings/config.py
@@ -5,3 +5,8 @@ OTC_TOOLKIT_PATH = abspath(join(DIR_PATH, '..'))
 PATH_TO_RECORDINGS = "data"
 STATIONS = ['ecdf', 'citylab']
 BOARDS = ['nano', 'tx2', 'xavier']
+
+COUNTER_LINE_COORDS = {'ecdf':
+                           {'nano': {'dir1': (), 'dir2': ()}, 'xavier': {'dir1': (), 'dir2': ()}},
+                       'citylab':
+                           {"point1": {"x": 34, "y": 740}, "point2": {"x": 1433, "y": 103}}}
